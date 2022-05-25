b72f31511b72a6d8c85cb70bce62aad7d30c32a8
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-05-24 20:39:43 -0700

    add nav jsx and styled nav for mobile size

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -4,6 +4,15 @@ export default function Nav() {
     return (
         <nav className="nav">
             <a href="/" className="nav__logo"></a>
+            <div className="nav__search-wrap">
+                <form>
+                    <label>
+                        <input className="nav__search" type="text" placeholder="Search" />
+                    </label>
+                </form>
+                <div className="nav__avatar"></div>
+            </div>
+            <button className="nav__button">upload</button>
         </nav>
     )
 }
