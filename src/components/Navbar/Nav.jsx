175fb77499b3619fab228b8e665d058aea148855
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-05-24 19:12:20 -0700

    add partial scss files(typefaces, reset, mixins, variables), add component folders with jsx and scss files, add svgs to images folder, modified folder names for better practice

---
+++ src/components/Navbar/Nav.jsx
@@ -0,0 +1,11 @@
+import "./Nav.scss";
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <a href="/" className="nav__logo"></a>
+        </nav>
+    )
+}
+
+
