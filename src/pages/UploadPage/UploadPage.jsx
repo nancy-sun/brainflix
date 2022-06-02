e3bd9f4e8f59be90372b66e428703155147c7b62
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-06-02 16:55:16 -0700

    add border styling global scss variable, add upload page structure and mobile size styling

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -2,6 +2,26 @@ import "./UploadPage.scss";
 
 export default function UploadPage() {
     return (
-        <div>upload</div>
+        <div className="upload">
+            <h1 className="upload__headline">Upload Video</h1>
+            <div className="upload__thumbnail-wrap">
+                <p className="upload__thumbnail-title">video thumbnail</p>
+                <div className="upload__thumbnail"></div>
+            </div>
+            <form className="upload__form">
+                <label htmlFor="title" className="upload__label">
+                    title your video
+                    <input type="text" name="title" id="title" placeholder="Add a title to your video" className="upload__title" />
+                </label>
+                <label htmlFor="description" className="upload__label"  >
+                    add a video description
+                    <textarea name="description" id="description" placeholder="Add a description to your video" className="upload__description"></textarea>
+                </label>
+                <div className="upload__buttons">
+                    <button className="upload__publish">publish</button>
+                    <button type="button" className="upload__cancel">cancel</button>
+                </div>
+            </form>
+        </div>
     )
 }
